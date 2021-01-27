54053adced26033637561a2c564e7fbfc1d3d5f8
Sebastian Hahne <sebastianhahne@web.de>
2021-01-27 18:14:32 +0100

    first steps

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <b-nav tabs>
+      <b-nav-item disabled>Active</b-nav-item>
+      <b-nav-item disabled>counter</b-nav-item>
+    </b-nav>
+  </div>
+</template>
---
+++ src/components/Quizbox.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <b-jumbotron header="" lead="Bootstrap v4 Components for Vue.js 2">
+      <template slot="lead">
+        question
+      </template>
+
+
+      <hr class="my-4">
+
+      <p>
+        list of answers
+      </p>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" href="#">Next</b-button>
+
+    </b-jumbotron>
+  </div>
+</template>
